2e83427f44a1354cc7a7faebae4297557933958d
sonu1239 <ss316742@gmail.com>
2022-04-17 20:40:15 +0530

    First Name

--- UC1_firstName.js
+++ UC1_firstName.js
@@ -1,1 +1,14 @@
 console.log("Welcome to user Registration");
+
+const regex = /^[A-Z]{1}[a-z]{2,}$/;
+let str = "sonu";
+
+let result = regex.exec(str);
+console.log("The result from exec is : ", result);
+
+if(regex.test(str)){
+    console.log(`The string ${str} matchs the expression ${regex.source}`);
+}
+else{
+    console.log(`The string ${str} does not match the expression ${regex.source}`);
+}
